ca8f149035cef428a8ce0ad82c0e04b3a3e31348
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-06 00:26:59 +0300

    Fix: close tag

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -10,7 +10,7 @@ const MenuItem = ({ title, imageUrl, size }) => {
 					style={{
 						backgroundImage: `url(${imageUrl})`,
 					}}
-				></div>
+				/>
 				<div className="content">
 					<h1 className="title">{title}</h1>
 					<span className="subtitle">SHOP NOW</span>

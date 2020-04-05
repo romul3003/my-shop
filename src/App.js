f54f66e691f7c34bfc3a9940e3e9939caa23b997
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-05 23:03:23 +0300

    Add files for home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import HomePage from './Homepage'
 
 function App() {
-	return <div className="App">Hey you!!!</div>
+	return (
+		<div className="App">
+			<HomePage />
+		</div>
+	)
 }
 
 export default App

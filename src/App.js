11e37ea9b5041dee67f1a6d9056e61ed64d26b11
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-06 22:53:22 +0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
 import HomePage from './pages/Homepage/Homepage'
 import ShopPage from './pages/ShopPage/ShopPage'
 import './App.css'
@@ -7,6 +8,7 @@ import './App.css'
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -6,7 +6,7 @@ import CollectionItem from '../CollectionItem/CollectionItem'
 const CollectionPreview = ({ title, items }) => {
 	return (
 		<div className="collection-preview">
-			<h1 className="Title">{title}</h1>
+			<h1 className="title">{title}</h1>
 			<div className="preview">
 				{items
 					.filter((item, idx) => idx < 4)

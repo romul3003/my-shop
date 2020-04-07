d0c3f8c98bb2458409291794db1d69e31d88d247
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-07 16:27:18 +0300

    Add SingIn component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/Homepage/Homepage'
 import ShopPage from './pages/ShopPage/ShopPage'
+import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp'
 import './App.css'
 
 function App() {
 	return (
 		<div>
 			<Switch>
-				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route exact path="/" component={HomePage} />
+				<Route path="/signin" component={SignInAndSignUp} />
 			</Switch>
 		</div>
 	)
---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SignInAndSignUp.scss'
+
+const SignInAndSignUp = () => {
+	return <div className="sign-in-and-sign-up">SIGN IN</div>
+}
+
+export default SignInAndSignUp

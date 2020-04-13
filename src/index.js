0ba19c2e6620b7d9860f0bcfa6dd34876a6420b5
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-14 00:11:38 +0300

    Add redux packages, rootReducer, userReducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )

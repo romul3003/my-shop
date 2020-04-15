a283f89d9d0e203f9c8d6b61fc73cbb6dd964610
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-15 23:17:41 +0300

    Add redux in Cart

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './CartIcon.scss'
-
 import { ReactComponent as ShoppingIcon } from '../../assets/icons/shopping-bag.svg'
+import { toggleCartHidden } from '../../redux/cart/cartActions'
 
-const CartIcon = () => {
+const CartIcon = ({ toggleCartHidden }) => {
   return (
-    <div className="cart-icon">
+    <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
   )
 }
 
-export default CartIcon
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon)
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import './Header.scss'
 import CartIcon from '../CartIcon/CartIcon'
 import CartDropdown from '../CartDropdown/CartDropdown'
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -31,13 +31,14 @@ const Header = ({ currentUser }) => {
         )}
         <CartIcon />
       </div>
-      <CartDropdown />
+      {hidden ? null : <CartDropdown />}
     </div>
   )
 }
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 })
 
 // const mapDispatchToProps = (dispatch) => ({})

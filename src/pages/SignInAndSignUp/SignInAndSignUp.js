b4a4be11f5dcccc912283082b6d0cd9bd7000f78
Roman Zakrevskyi <romul3003@gmail.com>
2020-05-28 20:47:19 +0300

    Refactore SignInAndSignUp component to styled components

--- src/pages/SignInAndSignUp/SignInAndSignUp.js
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import './SignInAndSignUp.scss'
 import SignIn from '../../components/SignIn/SignIn'
 import SignUp from '../../components/SignUp/SignUp'
 
-const SignInAndSignUp = () => {
-  return (
-    <div className="sign-in-and-sign-up">
-      <SignIn />
-      <SignUp />
-    </div>
-  )
-}
+import { SignInAndSignUpContainer } from './SignInAndSignUp.styles'
+
+const SignInAndSignUp = () => (
+  <SignInAndSignUpContainer>
+    <SignIn />
+    <SignUp />
+  </SignInAndSignUpContainer>
+)
 
 export default SignInAndSignUp

83126c5599bac8c25e9f0726e216b01459a0f631
Roman Zakrevskyi <romul3003@gmail.com>
2020-05-28 21:14:20 +0300

    Refactore CollectionPreview component to styled components

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import './CollectionPreview.scss'
-
+import { withRouter } from 'react-router-dom'
 import CollectionItem from '../CollectionItem/CollectionItem'
 
-const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className="collection-preview">
-      <h1 className="title">{title}</h1>
-      <div className="preview">
-        {items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))}
-      </div>
-    </div>
-  )
-}
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer,
+} from './CollectionPreview.styles'
 
-export default CollectionPreview
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
+  <CollectionPreviewContainer>
+    <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
+      {title.toUpperCase()}
+    </TitleContainer>
+    <PreviewContainer>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </PreviewContainer>
+  </CollectionPreviewContainer>
+)
+
+export default withRouter(CollectionPreview)

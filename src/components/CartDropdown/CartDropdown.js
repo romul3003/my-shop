a8b7932df827f87b71fed4af366869d506b0bb81
Roman Zakrevskyi <romul3003@gmail.com>
2020-05-28 20:52:53 +0300

    Refactore CartDropdown component to styled components

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -2,35 +2,38 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { withRouter } from 'react-router-dom'
-import CustomButton from '../CustomButton/CustomButton'
 import CartItem from '../CartItem/CartItem'
 import { selectCartItems } from '../../redux/cart/cartSelectors'
 import { toggleCartHidden } from '../../redux/cart/cartActions'
-import './CartDropdown.scss'
 
-const CartDropdown = ({ cartItems, history, dispatch }) => {
-  return (
-    <div className="cart-dropdown">
-      <div className="cart-items">
-        {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} item={cartItem} />
-          ))
-        ) : (
-          <span className="empty-message">Your cart is empty</span>
-        )}
-      </div>
-      <CustomButton
-        onClick={() => {
-          history.push('/checkout')
-          dispatch(toggleCartHidden())
-        }}
-      >
-        Go to checkout
-      </CustomButton>
-    </div>
-  )
-}
+import {
+  CartDropdownContainer,
+  CartDropdownButton,
+  EmptyMessageContainer,
+  CartItemsContainer,
+} from './CartDropdown.styles'
+
+const CartDropdown = ({ cartItems, history, dispatch }) => (
+  <CartDropdownContainer>
+    <CartItemsContainer>
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
+      )}
+    </CartItemsContainer>
+    <CartDropdownButton
+      onClick={() => {
+        history.push('/checkout')
+        dispatch(toggleCartHidden())
+      }}
+    >
+      GO TO CHECKOUT
+    </CartDropdownButton>
+  </CartDropdownContainer>
+)
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,

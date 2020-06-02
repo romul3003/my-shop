d81c5cc5b88f99453c01362b03ee2120106ecbe1
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-02 20:27:26 +0300

    Use firestore collectionRef.get() method instead of collectionRef.onSnapShot() - ShopPage

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -27,13 +27,19 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props
     const collectionRef = firestore.collection('collections')
 
-    this.unsubscribeFromSnapShot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-        updateCollections(collectionsMap)
-        this.setState({ loading: false })
-      }
-    )
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      updateCollections(collectionsMap)
+      this.setState({ loading: false })
+    })
+
+    // this.unsubscribeFromSnapShot = collectionRef.onSnapshot(
+    //   async (snapshot) => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+    //     updateCollections(collectionsMap)
+    //     this.setState({ loading: false })
+    //   }
+    // )
   }
 
   render() {

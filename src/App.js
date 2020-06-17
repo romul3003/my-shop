388dcd8312ae20deb38ff5586d1930d4edf20cc4
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-17 19:38:00 +0300

    Refactore: change App component from class to function, use hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+
 import { checkUserSession } from './redux/user/userActions'
+import { selectCurrentUser } from './redux/user/userSelectors'
+
 import Header from './components/Header/Header'
 import HomePage from './pages/Homepage/Homepage'
 import ShopPage from './pages/ShopPage/ShopPage'
 import CheckoutPage from './pages/Checkout/Checkout'
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp'
-import { selectCurrentUser } from './redux/user/userSelectors'
+
 import './App.css'
 
-class App extends React.Component {
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    const { checkUserSession } = this.props
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession()
-  }
+  }, [checkUserSession])
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth()
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() => {
-              return this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUp />
-              )
-            }}
-          />
-        </Switch>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() => {
+            return currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+          }}
+        />
+      </Switch>
+    </div>
+  )
 }
 
 const mapStateToProps = createStructuredSelector({

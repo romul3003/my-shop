4fdcdffe64524f6a95700e65e714a8f4e922021c
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-27 18:46:39 +0300

    Connect client to server

--- client/src/components/StripeButton/StripeButton.js
+++ client/src/components/StripeButton/StripeButton.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_fjfpthTPsmPn3elKJSSBokOS00PAGHb0EV'
 
   const onToken = (token) => {
-    // eslint-disable-next-line no-console
-    console.log(token)
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful')
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log('Payment error: ', JSON.parse(error))
+        alert(
+          'There was an issue with your payment. Please sure you use the provided credit card.'
+        )
+      })
   }
 
   return (
